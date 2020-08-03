379cb53367df4aae5f7c398f801a1ec89bc43669
菅原貫太 <>
2020-08-03 17:08:35 +0900

    no message

--- fashion_sample/label_image.py
+++ fashion_sample/label_image.py
@@ -26,7 +26,7 @@ import glob
 
 def load_graph(model_file):
   graph = tf.Graph()
-  graph_def = tf.GraphDef()
+  graph_def = tf.compat.v1.GraphDef()
 
   with open(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
